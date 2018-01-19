4eaa5ddb26047e495f1b1945a1ccedbd6ee60788
Anna Khamsamran <akamsamran1@cnm.edu>
2018-01-19 08:39:58 -0700

    added php for big bang scientist

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anna
+ * Date: 1/19/2018
+ * Time: 8:07 AM
+ */
+
+/*function name(string $name, float $stubbornness) :*/
+
+
+<?php
+class BigBangScientist {
+    public $name;
+    public $stubborness;
+
+    public function argue() : void {
+        echo "I AM RITE" . PHP_EOL;
+    }
+}
+
+$leonard = new BigBangScientist();
+$sheldon = new BigBangScientist();
+
+$leonard->name = "Leonard";
+$sheldon->name = "Sheldon";
+
+$leonard->stubborness = 0.72;
+$sheldon->stubborness = 0.999812;
+
+
+
+
+/*$name = "";
+function $whoIsStubborn(float $stubbornness, string $name): string {
+}*/
+/*}
+    $stubbornness = 0.0;
+    for($i =0; $i < $stubbornness; $i++) {
+
+if ($name = 'Sheldon', $stubbornness >=6){
+$name='Sheldon';
+$stubbornness = 7;
+echo "Sheldon is too stubborn!";
+}*/
+
+
+
+
+
+?>
---
+++ snap-big-bang.js
@@ -0,0 +1,4 @@
+function name(string $name, float $stubbornness) :
+
+}
+let
